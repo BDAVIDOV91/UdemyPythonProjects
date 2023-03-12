664024ce452cd5bd52738361dd10b47e76701813
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-03-12 11:57:52 +0200

    modify

--- Side_projects/aleks_birthday.py
+++ Side_projects/aleks_birthday.py
@@ -1,10 +1,12 @@
 from datetime import date
 
-
 today = date.today()
 aleks_birthday = date(today.year, 5, 8)
+
 if aleks_birthday < today:
     aleks_birthday = aleks_birthday.replace(year = today.year + 1)
+    
+    
 time_till_birthday = abs(aleks_birthday - today)
 
 # transform (PARSE) timetillbirthday.days to string (str)
