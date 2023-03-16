97b49c1a399d6a944f271fd40c9b2102212bd0eb
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-03-16 13:16:35 +0200

    adding and modifing

--- Web_development/script1.py
+++ Web_development/script1.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'TechnoJihad !'
+    return render_template('home.html')
 
 @app.route('/aboutLife/')
 def about():
-    return 'We Die - sadface !'
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
