1fe768d07fdb982a99f31ff7df95c503f7f16807
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-03-27 12:43:11 +0300

    modify/update

--- mobile_app_dev/main.py
+++ mobile_app_dev/main.py
@@ -5,7 +5,12 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = 'sign_up_screen'
+
+class SignUpScreen(Screen):
     pass
+
 class RootWidged(ScreenManager):
     pass
 
