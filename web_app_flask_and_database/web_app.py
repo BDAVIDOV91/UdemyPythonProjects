3e00b31d011d2bf23048ce017768261cbfc5a88f
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-04-05 16:29:50 +0300

    having bad time postgresql

--- web_app_flask_and_database/web_app.py
+++ web_app_flask_and_database/web_app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+from flask.ext.sqlalcemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/hight_collector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__ = 'data'
+    id = db.Column(db.Integer, prinary_key = True)
+    email_ = db.Column(db.String(120), unique = True)
+    height_ = db.Column(db.Integer)
+    
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
 
 @app.route('/')
 def index():
