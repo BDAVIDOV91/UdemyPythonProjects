3186c1337e8e6f8e41d9abb9cbd831527d146473
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-04-11 16:38:05 +0300

    advancing in web apps

---
+++ web_app_flask_and_database/send_email.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def send_email(email, height):
+    from_email = 'bgtscerebrate@gmail.com'
+    from_password = 'kfeilaeubbjbbmim'
+    to_email = email
+    
+    subject = 'Height data'
+    message = 'Hey there, your height is <strong>%s</strong>.' % height
+    
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = subject
+    msg['To'] = to_email
+    msg['From'] = from_email
+    
+    try:
+        # Connect to Gmail's SMTP server
+        gmail = smtplib.SMTP('smtp.gmail.com', 587)
+        gmail.ehlo()
+        gmail.starttls()
+        gmail.login(from_email, from_password)
+        
+         # Send the email
+        gmail.send_message(msg)
+        gmail.close()
+        print('Email sent successfully.')
+    except Exception as e:
+        print('Failed to send email.Error', str(e))
--- web_app_flask_and_database/web_app.py
+++ web_app_flask_and_database/web_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
 
 # Set up the Flask app and SQLAlchemy
 app = Flask(__name__)
@@ -27,7 +28,7 @@ def success():
     if request.method == 'POST':
         email = request.form['email_name']
         height = request.form['height_name']
-        print(email, height)
+        send_email(email, height)
         if db.session.query(Data).filter(Data.email_ == email).count() == 0:
             data = Data(email, height)
             db.session.add(data)
