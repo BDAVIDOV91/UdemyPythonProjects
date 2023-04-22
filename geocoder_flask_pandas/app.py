53ee81c65492a785571e27bf330c7dd878d72a2d
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-04-22 18:24:42 +0300

    finalizing the app / debugging

--- geocoder_flask_pandas/app.py
+++ geocoder_flask_pandas/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 from geopy.geocoders import Nominatim
 import pandas
+import datetime
 
 app = Flask(__name__)
 
@@ -10,20 +11,25 @@ def index():
 
 @app.route('/success-table', methods = ['POST'])
 def success_table():
+    global filename
     if request.method == 'POST':
         file = request.files['file']
-        df = pandas.read_csv(file)
-        gc = Nominatim()
-        df['coordinates'] = df['Address'].apply(gc.geocode)
-        df['Latitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
-        df['Longtitude'] = df['coordinates'].apply(lambda x: x.longtitude if x != None else None)
-        df = df.drop('coordinates', 1)
-        df.to_csv('upload/geocoded.csv', index = None)
-        return render_template('index.html', text = df.to_html(), btn = 'download.html')
+        try:
+            df = pandas.read_csv(file)
+            gc = Nominatim(user_agent='Legit Geocoder', scheme = 'http')
+            df["coordinates"] = df["Address"].apply(gc.geocode)
+            df['Latitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
+            df['Longitude'] = df['coordinates'].apply(lambda x: x.longitude if x != None else None)
+            df = df.drop('coordinates', axis = 1, inplace = True)
+            filename = datetime.datetime.now().strftime('uploeads/%Y-%m-%d-%H-%M-%S-%f' + '.csv')
+            df.to_csv(filename, index = None)
+            return render_template('index.html', text = df.to_html(), btn = 'download.html')
+        except Exception as e:
+            return render_template("index.html", text=str(e))
 
 @app.route('/download-file/')
 def download():
-    return send_file('upload/geocoded.csv', attachment_file = 'yourfile.csv', as_attachment = True)
+    return send_file(filename, attachment_file = 'yourfile.csv', as_attachment = True)
 
 if __name__== '__main__':
-    app.run(debug = True, port = 5005)
+    app.run(debug = True, port = 5005) # In my case port :5000 was busy so we can switch ports
