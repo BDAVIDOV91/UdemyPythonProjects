cf54655889936befcb96a74877e9d273e9d6f322
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-04-23 21:05:10 +0300

    little improvement

--- Side_projects/face_recognition/face_rec_cv2.py
+++ Side_projects/face_recognition/face_rec_cv2.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 # Load pre-trained face detection model
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -8,16 +9,18 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('model.yml')
 
-# Load images of yourself and create a dictionary of labels
-image_paths = ['image1.jpg', 'image2.jpg', 'image3.jpg']
-labels = [0, 0, 0] # All images are of yourself
+# Load images of yourself from a directory and create a dictionary of labels
 face_images = []
-for image_path in image_paths:
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30))
-    for (x, y, w, h) in faces:
-        face_images.append(gray[y : y + h, x : x + w])
+labels = []
+path = 'path/to/face/images/directory'
+for filename in os.listdir(path):
+    if filename.endswith('.jpg'):
+        image = cv2.imread(os.path.join(path, filename))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30))
+        for (x, y, w, h) in faces:
+            face_images.append(gray[y : y + h, x : x + w])
+            labels.append(len(face_images)-1)
     
 # Train face recognition model
 recognizer.train(face_images, np.array(labels))
