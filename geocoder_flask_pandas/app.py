9c2d310148dc003fcef5ebac4b5d4d867624a185
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-04-23 09:49:02 +0300

    ok we fixed the include method in the index.html file

--- geocoder_flask_pandas/app.py
+++ geocoder_flask_pandas/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from geopy.geocoders import Nominatim
-import pandas
+import pandas as pd
 import datetime
 
 app = Flask(__name__)
@@ -15,19 +15,19 @@ def success_table():
     if request.method == 'POST':
         file = request.files['file']
         try:
-            df = pandas.read_csv(file)
+            df = pd.read_csv(file)
             gc = Nominatim(user_agent='Legit Geocoder', scheme = 'http')
             df["coordinates"] = df["Address"].apply(gc.geocode)
             df['Latitude'] = df['coordinates'].apply(lambda x: x.latitude if x != None else None)
             df['Longitude'] = df['coordinates'].apply(lambda x: x.longitude if x != None else None)
             df = df.drop('coordinates', axis = 1, inplace = True)
             filename = datetime.datetime.now().strftime('uploeads/%Y-%m-%d-%H-%M-%S-%f' + '.csv')
-            df.to_csv(filename, index = None)
+            df.to_csv(filename, index=None)
             return render_template('index.html', text = df.to_html(), btn = 'download.html')
         except Exception as e:
             return render_template("index.html", text=str(e))
 
-@app.route('/download-file/')
+@app.route('/donwload-file/')
 def download():
     return send_file(filename, attachment_file = 'yourfile.csv', as_attachment = True)
 
