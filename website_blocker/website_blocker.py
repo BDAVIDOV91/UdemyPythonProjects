570693c2393d21c5369bac69a6d7ceb91d8d8ee1
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-05-01 15:43:26 +0300

    removing a typo

--- website_blocker/website_blocker.py
+++ website_blocker/website_blocker.py
@@ -2,14 +2,14 @@ import time
 from datetime import datetime as dt
 
 host_temp = 'hosts' # Fake hosts file created to test the script
-host_path = '/etc/hosts/' # The original hosts file.When you run it must be with admin privilege
+host_path = '/etc/hosts' # The original hosts file.When you run it must be with admin privilege
 redirect = '127.0.0.1'
 website_list = ['www.facebook.com', 'facebook.com', 'www.youtube.com', 'youtube.com',
                 'netflix.com', 'disneyplus.com', 'hbomax.com', 'zoro.to', 
                 'www.netflix.com', 'www.disneyplus.com', 'www.hbomax.com', 'www.zoro.to']
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 19):
         print('Working hours...')
         with open(host_path, 'r+') as file:
             content = file.read()
