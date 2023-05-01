d06bf0813c03b764162661fa074651c9f8d2c393
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-05-01 14:51:56 +0300

    implementing the first part

--- website_blocker/website_blocker.py
+++ website_blocker/website_blocker.py
@@ -1,13 +1,23 @@
 import time
 from datetime import datetime as dt
 
+host_temp = 'hosts'
 host_path = '/etc/hosts/'
 redirect = '127.0.0.1'
-website_list = ['www.facebook.com', 'facebook.com', 'www.youtube.com', 'youtube.com']
+website_list = ['www.facebook.com', 'facebook.com', 'www.youtube.com', 'youtube.com',
+                'netflix.com', 'disneyplus.com', 'hbomax.com', 'zoro.to', 
+                'www.netflix.com', 'www.disneyplus.com', 'www.hbomax.com', 'www.zoro.to']
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
         print('Working hours...')
+        with open(host_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + ' ' + website + '\n')
     else:
         print('It\'s fun hours... You are free to do whatever you want !')
     time.sleep(5)
