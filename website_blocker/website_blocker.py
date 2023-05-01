1941f1934a1d37ca5dd7c754a302818684431652
BlagovestDavidov <bgtscerebrate@gmail.com>
2023-05-01 15:31:33 +0300

    implementing the second part

--- website_blocker/website_blocker.py
+++ website_blocker/website_blocker.py
@@ -1,8 +1,8 @@
 import time
 from datetime import datetime as dt
 
-host_temp = 'hosts'
-host_path = '/etc/hosts/'
+host_temp = 'hosts' # Fake hosts file created to test the script
+host_path = '/etc/hosts/' # The original hosts file.When you run it must be with admin privilege
 redirect = '127.0.0.1'
 website_list = ['www.facebook.com', 'facebook.com', 'www.youtube.com', 'youtube.com',
                 'netflix.com', 'disneyplus.com', 'hbomax.com', 'zoro.to', 
@@ -11,7 +11,7 @@ website_list = ['www.facebook.com', 'facebook.com', 'www.youtube.com', 'youtube.
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
         print('Working hours...')
-        with open(host_temp, 'r+') as file:
+        with open(host_path, 'r+') as file:
             content = file.read()
             for website in website_list:
                 if website in content:
@@ -19,5 +19,13 @@ while True:
                 else:
                     file.write(redirect + ' ' + website + '\n')
     else:
-        print('It\'s fun hours... You are free to do whatever you want !')
+        with open(host_path, 'r+') as file:
+            lines = file.readlines()
+            file.seek(0)
+            for line in lines:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+            
+        print('It\'s fun time...')
     time.sleep(5)
